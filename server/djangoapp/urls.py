e28dc33ee966e64902d22e45d4de30d26c8de741
Abdalla Eldoumani <aamsdoumani@gmail.com>
2025-12-31 21:01:09 -0700

    ad

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,6 +14,8 @@ urlpatterns = [
     path(route='logout', view=views.logout_request, name='logout'),
     # path for registration
     path(route='registration', view=views.registration, name='registration'),
+    
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 
     # path for dealer reviews view
 
